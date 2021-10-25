c2cacfe8de74227778d5927273e0f524c4d446f4
unknown <akarim0517@gmail.com>
2021-10-24 21:55:51 -0400

    Challenge commit

---
+++ Assets/Scripts/HeartSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartSystem : MonoBehaviour
+{
+    public GameObject[] hearts;
+    private int life;
+    private bool dead;
+
+    private void Start()
+    {
+        life = hearts.Length;
+    }
+
+    void Update()
+    {
+        if (dead == true)
+        {
+            Debug.Log("DEAD!!!!");
+        }
+    }
+
+    public void TakeDamage(int d)
+    {
+       if (life >= 1)
+        {
+            life -= d;
+            Destroy(hearts[life].gameObject);
+            if (life < 1)
+            {
+                dead = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,12 +10,19 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
+    public Text winText;
+    
+    private bool facingRight = true;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -26,10 +33,47 @@ public class PlayerScript : MonoBehaviour
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
         if (Input.GetKey("escape"))
         {
             Application.Quit();
         }
+
+        if (facingRight == false && hozMovement > 0)
+        {
+             Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -39,8 +83,13 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector3(33.0f,0.0f,0.0f);
+            }
         }
     }
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
@@ -49,7 +98,21 @@ public class PlayerScript : MonoBehaviour
             if(Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 3),ForceMode2D.Impulse);
+                anim.SetInteger("State", 3);
             }
+
         }
     }
+
+    void SetScoreText()
+    {
+        score.text = scoreValue.ToString();
+        if(scoreValue == 4)
+        {
+            winText.text = "You're DONE!";
+        }
+
+    }
 }
+
+
